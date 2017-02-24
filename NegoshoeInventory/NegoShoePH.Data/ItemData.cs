ca6edb73d68c67aae0f5072a2e8a8408f40d0c71
meanprogrammer <mean.programmer.d@gmail.com>
2017-02-24 10:59:03 +0800

    new

---
+++ NegoshoeInventory/NegoShoePH.Data/ItemData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NegoShoePH.Data
+{
+    public class ItemData
+    {
+        InventoryDataDataContext context = null;
+        public ItemData() {
+            context = new InventoryDataDataContext();
+        }
+
+        public List<Item> GetAllItem()
+        {
+            return context.Items.ToList();
+        }
+    }
+}
---
+++ NegoshoeInventory/NegoShoePH.Entity/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NegoShoePH.Entity
+{
+    public class Item
+    {
+        public int RecordID { get; set; }
+        public string ItemName { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public string Remarks { get; set; }
+    }
+}
