42c7e6b17db4ee543e2e4036276c9db5660a6122
meanprogrammer <mean.programmer.d@gmail.com>
2017-03-01 12:04:03 +0800

    unit test

--- NegoshoeInventory/NegoShoePH.DataTest/UnitTest1.cs
+++ NegoshoeInventory/NegoShoePH.DataTest/UnitTest1.cs
@@ -8,6 +8,44 @@ namespace NegoShoePH.DataTest
     [TestClass]
     public class UnitTest1
     {
+        InventoryDataDataContext context = new InventoryDataDataContext();
+        [TestInitialize()]
+        public void CreateData()
+        {
+            context.ExecuteCommand("truncate table Items");
+
+            context.Items.InsertOnSubmit(
+                new Item() { 
+                    ItemName = "Jordan 2 Wings Breds",
+                    Quantity = 10,
+                    Description = string.Empty,
+                    Remarks = string.Empty
+                }
+                );
+
+            context.SubmitChanges();
+        }
+
+        [TestCleanup()]
+        public void CleanUp()
+        {
+            context.ExecuteCommand("truncate table Items");
+        }
+
+        [TestMethod]
+        public void InsertTest()
+        {
+            Item i = new Item();
+            i.ItemName = "Jordan Banned Breds Snapback";
+            i.Quantity = 5;
+
+            context.Items.InsertOnSubmit(i);
+            var count = context.GetChangeSet().Inserts.Count;
+            context.SubmitChanges();
+
+            Assert.AreEqual(1, count);
+
+        }
 
         [TestMethod]
         public void GetAllItemsTest()
@@ -15,7 +53,7 @@ namespace NegoShoePH.DataTest
             using (InventoryDataDataContext context = new InventoryDataDataContext())
             {
                 Table<Item> data = context.Items;
-             
+                Assert.IsNotNull(data);
             }
         }
     }
