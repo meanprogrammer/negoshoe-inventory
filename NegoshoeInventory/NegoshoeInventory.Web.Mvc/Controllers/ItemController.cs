7084c789fc5dd6851f04ed516ab6562ea93fb386
meanprogrammer <mean.programmer.d@gmail.com>
2017-03-08 17:48:51 +0800

    ewe

--- NegoshoeInventory/NegoshoeInventory.Web.Mvc/Controllers/ItemController.cs
+++ NegoshoeInventory/NegoshoeInventory.Web.Mvc/Controllers/ItemController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using NegoShoePH.Data;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,6 +10,11 @@ namespace NegoshoeInventory.Web.Mvc.Controllers
 {
     public class ItemController : Controller
     {
+        ItemData data;
+        public ItemController() {
+            data = new ItemData();
+        }
+
         // GET: Item
         public ActionResult Index()
         {
@@ -28,11 +35,19 @@ namespace NegoshoeInventory.Web.Mvc.Controllers
 
         // POST: Item/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Create(FormCollection collection, HttpPostedFileBase ImageUrl)
         {
             try
             {
-                // TODO: Add insert logic here
+                var files = Request.Files;
+                var ext = Path.GetExtension(ImageUrl.FileName);
+                var filename = Path.GetFileNameWithoutExtension(ImageUrl.FileName);
+                var completeFilename = string.Format("{0}_{1}{2}", filename, Guid.NewGuid().ToString(), ext);
+                var uploadspath = Server.MapPath("uploads");
+                ImageUrl.SaveAs(string.Format("{0}\\{1}",uploadspath,completeFilename));
+
+                Item i = new Item();
+
 
                 return RedirectToAction("Index");
             }
